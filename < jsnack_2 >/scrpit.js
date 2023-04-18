445313127bb4fe365e5c484daeb4e420c3730598
alessandropecchini99 <pecchini99@gmail.com>
2023-04-18 17:48:28 +0200

    finish JSnack_2

--- < jsnack_2 >/scrpit.js
+++ < jsnack_2 >/scrpit.js
@@ -1,1 +1,35 @@
 // DESCRIZIONE ESERCIZIO
+// 1. Creare lista nomi e cognomi
+// 2. generare una lista di invitati mischiando nomi e cognomi
+// 3. Mostrare la lista a schermo
+
+
+
+// ------------------------------------------------------ //
+
+
+
+// 1. ARRAY
+let nomi = ['Alessandro', 'Lisa', 'Simone', 'Martina', 'Chiara', 'Letizia', 'Domenico', 'Omar'];
+
+let cognomi = ['Pecchini', 'Gasparini', 'Terzo', 'Buono', 'Salatini', 'Pini', 'Simbari', 'Bayadi']
+
+
+// 2. GENERARE NOMI RANDOMICAMENTE
+let nome = nomi[Math.floor(Math.random() * nomi.length)];
+let cognome = cognomi[Math.floor(Math.random() * cognomi.length)];
+
+
+// 3. MOSTRARE IL RISULTATO
+console.log(nome + ` ` + cognome);
+
+document.getElementById(`nome_generato`).innerHTML= nome + ` ` + cognome;
+
+
+// ------------------------------
+
+
+// ALTRO
+function refreshPage(){
+    window.location.reload();
+} 
